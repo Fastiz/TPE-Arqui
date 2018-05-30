9deaa4286aba56338dad3bbaf8d135158867c5dd
Fastiz <fastiz@itba.edu.ar>
2018-05-30 17:57:23 -0300

    Add files via upload

---
+++ Userland/SampleCodeModule/stdio.c
@@ -0,0 +1,19 @@
+#include "console.h"
+#include "videoDriver.h"
+#include ""
+
+extern int line, linePosition;
+
+void printf(char* format, ...){
+	int i = 0, parameterIndex;
+	while(format != 0){
+		if(format++ == '%'){
+			if(format == 'd')
+
+		}
+	}
+}
+
+void putchar(char c){
+	
+}
