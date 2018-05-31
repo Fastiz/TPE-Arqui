d7fabbe2d6b831c7194b7f57c7043492e0a3bd3c
fbaliarda <33381234+fbaliarda@users.noreply.github.com>
2018-05-31 18:52:22 -0300

    irqDispatcher ahora con arreglo de interrupciones.

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -2,19 +2,17 @@
 #include <keyboard_driver.h>
 #include <stdint.h>
 
+
+typedef void (* handler_ptr)();
+
 static void int_20();
 static void int_21();
 
+
+handler_ptr irqHandlers[] = {int_20,int_21};
+
 void irqDispatcher(uint64_t irq) {
-	switch (irq) {
-		case 0:
-			int_20();
-			break;
-		case 1:
-			int_21();
-			break;
-	}
-	return;
+	irqHandlers[irq]();
 }
 
 void int_20() {
