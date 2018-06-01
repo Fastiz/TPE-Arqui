06e164eac0585ea9c408703f45462c97e3658830
Miguel <mdiluca@itba.edu.ar>
2018-05-31 21:46:15 -0300

    fix facu

--- Kernel/videoDriver.c
+++ Kernel/videoDriver.c
@@ -50,11 +50,7 @@ struct vesa_mode {
 		return screen->height;
 	}
 
-<<<<<<< Updated upstream
-	struct RGB readPixel(uint64_t width, uint64_t height) {
-=======
-	/*struct RGB readPixel(int width, int height) {
->>>>>>> Stashed changes
+	struct RGB readPixel(int width, int height) {
 		struct RGB color = {0,0,0};
 		if(!(width > screen->width || height > screen->height || width < 0 || height < 0)) {
 			int pixelIndex = width + height*(screen->width);
@@ -64,7 +60,7 @@ struct vesa_mode {
 		    color.blue=*(pixelPos);
 	    }
 	    return color;
-	}*/
+	}
 
 	void writePixel(uint64_t width, uint64_t height, struct RGB color){
 		if(width > screen->width || height > screen->height || width < 0 || height < 0)
@@ -76,79 +72,3 @@ struct vesa_mode {
 		*(pixelPos+1) = color.green;
 		*(pixelPos) = color.blue;
 	}
-<<<<<<< HEAD
-
-	void writeBlock(uint64_t width, uint64_t height, struct RGB color, uint64_t size){
-		for(int i = width; i < width+size; i++){
-			for(int j = height; j < height+size; j++){
-				writePixel(i, j, color);
-			}
-		}
-	}
-
-<<<<<<< Updated upstream
-	void writeChar(char c, uint64_t x, uint64_t y, struct RGB color, uint64_t size){
-	 	if(c < 32 || c > 255) //falta ver el limite de c, no es 255 en esta fuente
-=======
-	/*void writeChar(char c, int x, int y, struct RGB color, int size){
-	 	if(c < 32 || c > 255)
->>>>>>> Stashed changes
-	 		return;
-	 	char * posOfChar = getCharPos(c);
-		for(int j = 0; j < charHeight ; j++) {
-			for(int i = 0, k = 128; i < charWidth; i++, k/=2){
-				if(k & posOfChar[j])
-					writeBlock(i*size + x, j*size + y, color,size);
-		 	}
-		}
-	}*/
-
-<<<<<<< Updated upstream
-	void writeString(char* string, uint64_t x, uint64_t y, struct RGB color, uint64_t size){
-=======
-	/*void writeString(char* string, int x, int y, struct RGB color, int size){
->>>>>>> Stashed changes
-	 	while(*string != 0){
-	 		writeChar(*string, x, y, color, size);
-	 		x += (charWidth + 1) * size;
-	 		string++;
-	 	}
-	}*/
-
-	void fillScreen(struct RGB color){
-	 	for(int y = 0; y < getHeight(); y++) {
-	 		for(int x = 0; x < getWidth(); x++)
-	 			writePixel(x,y,color);
-	 	}
-	}
-
-	void clearScreen() {
-	 	struct RGB black={0,0,0};
-	 	fillScreen(black);
-	}
-
-	void moveScreenUp(uint64_t size, uint64_t ammount, struct RGB background) {
-		for(int y = charHeight * size * ammount; y < screen->height; y++) {
-			for(int x = 0; x < screen->width; x++) {
-				writePixel(x, y - (charHeight * size * ammount), readPixel(x,y));
-				if(y >= screen->height - (charHeight * size * ammount))
-					writePixel(x, y, background);
-			}
-		}
-	}
-
-	void backupScreen() {
-		for(int y = 0; y < getHeight(); y++) {
-	 		for(int x = 0; x < getWidth(); x++)
-	 			backup[x][y] = readPixel(x,y);
-	 	}
-	}
-
-	void restoreScreen() {
-		for(int y = 0; y < getHeight(); y++) {
-	 		for(int x = 0; x < getWidth(); x++)
-	 			writePixel(x,y,backup[x][y]);
-	 	}
-	}
-=======
->>>>>>> 56c86820cab510dee87cfcb77736ad3f364c72b7
