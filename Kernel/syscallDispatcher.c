e5d51a1debe5cfed4dd065b67d72875676109a93
Miguel <mdiluca@itba.edu.ar>
2018-05-31 22:55:14 -0300

    syscalls

--- Kernel/syscallDispatcher.c
+++ Kernel/syscallDispatcher.c
@@ -1,10 +1,35 @@
 #include <stdint.h>
-#include <videoDriver.h>
+#include "videoDriver.h"
 
 
 typedef (uint64_t(*func)(uint64_t, uint64_t, uint64_t, uint64_t));
-static const func handlers[] = {&getWidth, &getHeight, &writePixel, &writeBlock};
 
-void syscallDispatcher(uint64_t offset){
-  handlers[offset]();
+uint64_t _writePixel(uint64_t width, uint64_t height, uint64_t color, uint64_t trash1){
+  writePixel(width, height, (struct RGB) color);
+  return 0;
+}
+
+uint64_t _readPixel(uint64_t width, uint64_t height, uint64_t trash1, uint64_t trash2){
+  return (uint64_t) readPixel(width, height);
+}
+
+uint64_t _getWidth(uint64_t trash1, uint64_t trash2, uint64_t trash3, uint64_t trash4){
+  return getWidth();
+}
+
+uint64_t _getHeight(uint64_t trash1, uint64_t trash2, uint64_t trash3, uint64_t trash4){
+  return getHeight();
+}
+
+uint64_t _writeBuffer(uint64_t index, uint64_t str, uint64_t trash1, uint64_t trash2){
+  return writeBuffer(index, (char *) str);
+}
+
+uint64_t _readBuffer(uint64_t index, uint64_t trash1, uint64_t trash2, uint64_t trash3){
+  return (uint64_t)readBuffer(index);
+}
+
+uint64_t _clearBuffer(uint64_t index, uint64_t trash1, uint64_t trash2, uint64_t trash3){
+  clearBuffer(index);
+  return 0;
 }
