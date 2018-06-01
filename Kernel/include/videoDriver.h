bde114257ae187a7047db0baa36ca91d346871e4
Miguel <mdiluca@itba.edu.ar>
2018-05-31 21:52:46 -0300

    Todas con uint64

--- Kernel/include/videoDriver.h
+++ Kernel/include/videoDriver.h
@@ -1,4 +1,4 @@
 uint64_t getWidth(uint64_t trash1, uint64_t trash2, uint64_t trash3, uint64_t trash4)
 uint64_t getHeight(uint64_t trash1, uint64_t trash2, uint64_t trash3, uint64_t trash4)
 uint64_t readPixel(uint64_t width, uint64_t height, uint64_t trash1, uint64_t trash2)
-uint64_t writePixel(uint64_t width, uint64_t height, uint64_t RGB color, uint64_t trash1)
+uint64_t writePixel(uint64_t width, uint64_t height, uint64_t color, uint64_t trash1)
