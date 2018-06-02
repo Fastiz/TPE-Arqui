0f25c5584599a6aba042153d09b430bd1dcbd43f
fastiz <facuastiz@gmail.com>
2018-06-02 15:10:57 -0300

    lee de stdout y stderr. desprolijo pero anda

--- Userland/SampleCodeModule/console.c
+++ Userland/SampleCodeModule/console.c
@@ -15,7 +15,7 @@ int bufferIndex =0;
 
 static const char* consoleName = "Consola\\::";
 
-static const struct RGB consoleBackground = {100,100,100};
+static const struct RGB consoleBackground = {0,0,0};
 static const struct RGB consoleColor = {0,0,255};
 static const struct RGB STDOUTColor = {255, 255, 255};
 static const struct RGB STDINColor = {255, 255, 255};
@@ -35,17 +35,34 @@ void console(){
 
 void init(){
   fillScreen(consoleBackground);
+  char * texto = "ESTO ES UN ERROR";
+  for(;*texto != 0;texto++){
+    _syscall(_write, 2, *texto);
+  }
+texto = "ESTO ES UN MENSAJE";
+  for(;*texto != 0;texto++){
+    _syscall(_write, 1, *texto);
+  }
 }
 
 void consoleLoop(){
   while(1){
     readStdin();
+    readStdout();
+    readStderr();
   }
 
 }
 
+int callCommand(){
+  //Llamar comando
+
+  //Limpiar buffer de entrada
+}
+
 void readStdin(){
 
+  bufferIndex=0;
   char * text = consoleName;
   while(*text){
     writeChar(*text, linePosition*letterSize*LETTER_SPACE, line*letterSize*ROW_HEIGHT, consoleColor, letterSize);
@@ -62,10 +79,35 @@ void readStdin(){
       buffer[bufferIndex++] = c;
       writeChar(c, linePosition*letterSize*LETTER_SPACE, line*letterSize*ROW_HEIGHT, STDINColor, letterSize);
       linePosition++;
+
     }
   }
   newLine();
-  bufferIndex=0;
+  callCommand();
+}
+
+void readStdout(){
+  int flag;
+  char c;
+  while(c=_syscall(_read, 1)){
+    flag=1;
+    writeChar(c, linePosition*letterSize*LETTER_SPACE, line*letterSize*ROW_HEIGHT, STDOUTColor, letterSize);
+    linePosition++;
+  }
+  if(flag)
+    newLine();
+}
+
+void readStderr(){
+  int flag;
+  char c;
+  while(c=_syscall(_read, 2)){
+    flag=1;
+    writeChar(c, linePosition*letterSize*LETTER_SPACE, line*letterSize*ROW_HEIGHT, STDERRColor, letterSize);
+    linePosition++;
+  }
+  if(flag)
+    newLine();
 }
 
 void newLine(){
