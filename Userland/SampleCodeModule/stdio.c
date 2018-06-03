737db7598be8f1c74b4f3bb522b59c27163b9f5c
fbaliarda <33381234+fbaliarda@users.noreply.github.com>
2018-06-03 17:20:08 -0300

    Agregado scanf.

--- Userland/SampleCodeModule/stdio.c
+++ Userland/SampleCodeModule/stdio.c
@@ -1,5 +1,6 @@
 #include "syscall.h"
 #include "stdio.h"
+#include "stdlib.h"
 #include <stdarg.h>
 
 void printf(char* format, ...){
@@ -7,9 +8,9 @@ void printf(char* format, ...){
   va_start(arg, format);
   int i;
   char* s;
-	for(format; *format != 0; format++){
-		if(*format == '%'){
-			format++;
+  for(format; *format != 0; format++){
+    if(*format == '%'){
+      format++;
       switch(*format){
             case 'c' :
               i = va_arg(arg,int);
@@ -22,33 +23,74 @@ void printf(char* format, ...){
                 i = -i;
                 putchar('-');
               }
-          int j=1;
-          for(;i>=j;j*=10){};
+              char num[21] = {0};
+              intToChar(i,10,num);
+              int i;
+              for(i = 0; num[i] != 0; i++)
+                putchar(num[i]);
+             break;
 
-          for(j/=10; j > 0; j/=10){
-            putchar(i/j+'0');
-            i%=j;
-          }
-
-          break;
-
-          case 's':
-            s = va_arg(arg,char*);
-            while(*s){
-              putchar(*s);
-              s++;
-            }
-            break;
-        }
-      }else{
-        putchar(*format);
+            case 's':
+              s = va_arg(arg,char*);
+              while(*s){
+                putchar(*s);
+                s++;
+              }
+              break;
       }
-
-     }
+    }else{
+      putchar(*format);
+    }
+  }
 }
 
+int scanf(char* format, ...){
+  va_list arg;
+  va_start(arg, format);
+  int i = 0;
+  char c;
+  int reading = 1;
+  void * loadValue;
+  int valuesLoaded = 0;
+  int number = 0;
 
+  while((c = getchar()) != '\n' && c != 0){
+    if(!reading && c == format[0])
+      reading = 1;
+    if(reading){
+      if(format[i] == '%'){
+        loadValue = va_arg(arg,void *);
+        switch(format[i+1]){
+            case 'c' :
+              *(char *)loadValue = c;
+              break;
+            case 'd' :
+              do{
+                number *= 10;
+                number += (c - '0');
+              }while((c = getchar()) >= '0' && c <= '9');
 
+              *(int *)loadValue = number;
+              number = 0;
+              break;
+            case 's':
+              do{
+                *(char *)loadValue = c;
+                loadValue++; 
+              }while((c = getchar()) != ' ' && c != '\n' && c != 0);
+              break;
+        }
+        valuesLoaded++;
+      }
+      else if(c != format[i]){
+        reading = 0;
+        continue;
+      }
+      i++;
+    }
+  }
+  return valuesLoaded;
+}
 void putchar(char c){
   _syscall(_write, 1, c);
 }
