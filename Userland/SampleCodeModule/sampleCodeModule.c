38a8840a527bf7472875c4de57f15f09f2d7445a
fbaliarda <33381234+fbaliarda@users.noreply.github.com>
2018-06-04 21:52:47 -0300

    Add files via upload

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -6,12 +6,8 @@
 static int var1 = 0;
 static int var2 = 0;
 
-
 int main() {
-	/*struct RGB color2 = {255,0,0};
-	_syscall(4,0,0,&color2);
-	_syscall(3, 200,200,color2,0);
-	_syscall(3, 500, 500,color2,0);*/
+	
 	console();
 
 	//Test if BSS is properly set up
