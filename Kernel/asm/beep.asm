4ee56ca429b4dfb8e383361ea36120cfe5b9d6d8
fastiz <facuastiz@gmail.com>
2018-06-05 15:00:13 -0300

    prueben el beep

--- Kernel/asm/beep.asm
+++ Kernel/asm/beep.asm
@@ -1,8 +1,51 @@
-GLOBAL beep
+;GLOBAL beep
 
+;section .text
+
+;beep:
+;  mov al, 1
+;  out 0x61, al
+;  ret
+
+  GLOBAL beep
+GLOBAL unBeep
 section .text
 
 beep:
-  mov al, 1
-  out 0x61, al
-  ret
+    push rbp
+    mov rbp, rsp
+
+    mov al, 0xb6
+    out 0x43, al
+    mov rax, 0
+    mov al, 11931
+    out 0x42, al
+    mov al, ah
+    out 0x42, al
+    in al, 0x61
+    or al, 0x03
+    out 61h, al
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
+unBeep:
+;call .pause1
+    in al, 0x61
+    and al, 0xFC
+    out 0x61, al
+    ret
+
+
+.pause1:
+    mov ecx, 65535
+.pause2:
+    dec ecx
+    jne .pause2
+    dec ebx
+    jne .pause1
+    in al, 0x61 ;apaga la nota
+    and al, 0xFC ;apaga speaker
+    out 0x61, al
+    ret
